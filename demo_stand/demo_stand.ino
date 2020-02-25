2d0c72a3498ce95d68fd177dc9adcb2f5fb043ed
Sergejs Koreskovs <skoreskovs@evolutiongaming.com>
2020-02-25 15:12:53 +0200

    123

--- demo_stand/demo_stand.ino
+++ demo_stand/demo_stand.ino
@@ -1,32 +1,56 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#define HEAT_TEMP 25
+
+#define RELAY_1 10
+#define RELAY_2 11
+#define RELAY_3 12
+#define RELAY_4 13
+
 // temperature
 
-#define ONE_WIRE_BUS 2
+#define FLOW_METER_BUS_1 2
+#define FLOW_METER_BUS_2 3
+
+#define ONE_WIRE_BUS_1 4
+#define ONE_WIRE_BUS_2 7
 #define TEMPERATURE_PRECISION 9
 
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire oneWire1(ONE_WIRE_BUS_1);
+OneWire oneWire2(ONE_WIRE_BUS_2);
 
-DallasTemperature sensors(&oneWire);
+DallasTemperature sensors1(&oneWire1);
+DallasTemperature sensors2(&oneWire2);
 
-DeviceAddress addresses[4] = {
-  { 0x28, 0x1D, 0x39, 0x31, 0x2, 0x0, 0x0, 0xF0 },
-  { 0x28, 0x1D, 0x39, 0x31, 0x2, 0x0, 0x0, 0xF0 },
-  { 0x28, 0x1D, 0x39, 0x31, 0x2, 0x0, 0x0, 0xF0 },
-  { 0x28, 0x1D, 0x39, 0x31, 0x2, 0x0, 0x0, 0xF0 }
+DeviceAddress addresses1[2] = {
+  { 0x28, 0xAA, 0x80, 0x16, 0x56, 0x14, 0x01, 0x54 },
+  { 0x28, 0xAA, 0xF6, 0x2E, 0x56, 0x14, 0x01, 0xF1 }
+};
+
+DeviceAddress addresses2[2] = {
+  { 0x28, 0xAA, 0xF9, 0xED, 0x3F, 0x14, 0x01, 0x04 },
+  { 0x28, 0xAA, 0x7F, 0xBA, 0x56, 0x14, 0x01, 0x26 }
 };
 
 // flow
 
-volatile int flow_frequency;
-unsigned int l_hour;
-unsigned char flowsensor = 2;
-unsigned long currentTime;
-unsigned long cloopTime;
+volatile int flow1_frequency;
+volatile int flow2_frequency;
 
-void flow () {
-   flow_frequency++;
+unsigned int l_hour1;
+unsigned int l_hour2;
+
+unsigned long currentTime;
+unsigned long cloopTime1;
+unsigned long cloopTime2;
+
+void flow1 () {
+   flow1_frequency++;
+}
+
+void flow2 () {
+   flow2_frequency++;
 }
 
 // Pump 
@@ -35,36 +59,69 @@ void flow () {
 #define START_DUTY_CYCLE 100
 
 void setup(void) {
+
+  pinMode(RELAY_1, OUTPUT);
+  digitalWrite(RELAY_1, HIGH);
+  pinMode(RELAY_2, OUTPUT);
+  digitalWrite(RELAY_2, HIGH);
+  pinMode(RELAY_3, OUTPUT);
+  digitalWrite(RELAY_3, HIGH);
+  pinMode(RELAY_4, OUTPUT);
+  digitalWrite(RELAY_4, HIGH);
+  
   Serial.begin(115200);
   Serial.println("Sensors deamo board starting...");
 
   // temperature
-  sensors.begin();
+  sensors1.begin();
+  sensors2.begin();
 
   Serial.print("Locating temperature sensors...");
   Serial.print("Found ");
-  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.print(sensors1.getDeviceCount(), DEC);
+  Serial.print(sensors2.getDeviceCount(), DEC);
   Serial.println(" devices");
 
   Serial.print("Temperature sensors parasite power is: ");
-  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  if (sensors1.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+  if (sensors2.isParasitePowerMode()) Serial.println("ON");
   else Serial.println("OFF");
 
-  for (byte i = 0; i < sizeof(addresses); i++) {
-    if (!sensors.getAddress(addresses[i], i)) Serial.println("Unable to find address for temperature sensor " + i);
+  for (byte i = 0; i < sizeof(addresses1); i++) {
+    if (!sensors1.getAddress(addresses1[i], i)) Serial.println("Unable to find address for temperature sensor " + i);
 
     Serial.print("Temperature sensor ");
     Serial.print(i);
     Serial.print(" address: ");
-    printAddress(addresses[i]);
+    printAddress(addresses1[i]);
     Serial.println();
 
-    sensors.setResolution(addresses[i], TEMPERATURE_PRECISION);
+    sensors1.setResolution(addresses1[i], TEMPERATURE_PRECISION);
 
     Serial.print("Temperature sensor ");
     Serial.print(i);
     Serial.print(" resolution: ");
-    Serial.print(sensors.getResolution(addresses[i]), DEC);
+    Serial.print(sensors1.getResolution(addresses1[i]), DEC);
+    Serial.println();
+    Serial.println("Temperature sensors OK");
+  }
+
+  for (byte i = 0; i < sizeof(addresses2); i++) {
+    if (!sensors2.getAddress(addresses2[i], i)) Serial.println("Unable to find address for temperature sensor " + i);
+
+    Serial.print("Temperature sensor ");
+    Serial.print(i);
+    Serial.print(" address: ");
+    printAddress(addresses2[i]);
+    Serial.println();
+
+    sensors2.setResolution(addresses2[i], TEMPERATURE_PRECISION);
+
+    Serial.print("Temperature sensor ");
+    Serial.print(i);
+    Serial.print(" resolution: ");
+    Serial.print(sensors2.getResolution(addresses2[i]), DEC);
     Serial.println();
     Serial.println("Temperature sensors OK");
   }
@@ -72,12 +129,16 @@ void setup(void) {
   // flow
 
   Serial.println("Flow sensor setup...");
-  pinMode(flowsensor, INPUT);
-  digitalWrite(flowsensor, HIGH);
-  attachInterrupt(0, flow, RISING);
+  pinMode(FLOW_METER_BUS_1, INPUT);
+  pinMode(FLOW_METER_BUS_2, INPUT);
+  digitalWrite(FLOW_METER_BUS_1, HIGH);
+  digitalWrite(FLOW_METER_BUS_2, HIGH);
+  attachInterrupt(0, flow1, RISING);
+  attachInterrupt(1, flow2, RISING);
   sei();
   currentTime = millis();
-  cloopTime = currentTime;
+  cloopTime1 = currentTime;
+  cloopTime2 = currentTime;
   Serial.println("Flow sensors OK");
 
   // Pump 
@@ -97,23 +158,23 @@ void printAddress(DeviceAddress deviceAddress) {
   }
 }
 
-void printTemperature(DeviceAddress deviceAddress) {
-  float tempC = sensors.getTempC(deviceAddress);
+void printTemperature(DallasTemperature sensor, DeviceAddress deviceAddress) {
+  float tempC = sensor.getTempC(deviceAddress);
   Serial.print("Temp C: ");
   Serial.print(tempC);
 }
 
-void printResolution(DeviceAddress deviceAddress) {
+void printResolution(DallasTemperature sensor, DeviceAddress deviceAddress) {
   Serial.print("Resolution: ");
-  Serial.print(sensors.getResolution(deviceAddress));
+  Serial.print(sensor.getResolution(deviceAddress));
   Serial.println();
 }
 
-void printData(DeviceAddress deviceAddress) {
+void printData(DallasTemperature sensor, DeviceAddress deviceAddress) {
   Serial.print("Device Address: ");
   printAddress(deviceAddress);
   Serial.print(" ");
-  printTemperature(deviceAddress);
+  printTemperature(sensor, deviceAddress);
   Serial.println();
 }
 
@@ -135,23 +196,41 @@ void loop(void) {
   }
   
   // flow
-
+  boolean flag = false;
   currentTime = millis();
-  if(currentTime >= (cloopTime + 1000))
+  if(currentTime >= (cloopTime1 + 1000))
   {
-    cloopTime = currentTime;
-    l_hour = (flow_frequency * 60 / 7.5);
-    flow_frequency = 0;
-    Serial.println("Flow sensors data:");
-    Serial.print(l_hour, DEC);
+    cloopTime1 = currentTime;
+    l_hour1 = (flow1_frequency * 60 / 7.5);
+    flow1_frequency = 0;
+    Serial.println("Flow 1 sensors data:");
+    Serial.print(l_hour1, DEC);
     Serial.println(" L/hour");
-     
-    // temperature
-  
-    sensors.requestTemperatures();
-    Serial.println("Temperature sensors data:");
-    for (int i = 0; i < sizeof(addresses); i++) {
-      printData(addresses[i]);
-    }
+    flag = true;
+  }
+  if(currentTime >= (cloopTime2 + 1000))
+  {
+    cloopTime2 = currentTime;
+    l_hour2 = (flow2_frequency * 60 / 7.5);
+    flow2_frequency = 0;
+    Serial.println("Flow 2 sensors data:");
+    Serial.print(l_hour2, DEC);
+    Serial.println(" L/hour");
+    flag = true;
+  }
+ 
+  // temperature
+  if (flag) {
+      sensors1.requestTemperatures();
+      Serial.println("Temperature sensors data:");
+      for (int i = 0; i < sizeof(addresses1); i++) {
+        printData(sensors1, addresses1[i]);
+      }
+      sensors2.requestTemperatures();
+      Serial.println("Temperature sensors data:");
+      for (int i = 0; i < sizeof(addresses2); i++) {
+        printData(sensors2, addresses2[i]);
+      }
+      flag = false;
   }
 }
